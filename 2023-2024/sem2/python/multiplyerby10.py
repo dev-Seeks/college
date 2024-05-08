c1394cb6815b48177c6dce44149072c3d4b592f9
Xavier Corrales <168383006+dev-Seeks@users.noreply.github.com>
2024-05-08 15:49:53 -0600

    Create multiplyerby10.py

---
+++ 2023-2024/sem2/python/multiplyerby10.py
@@ -0,0 +1,10 @@
+product =float(input("Enter A Number: "))
+
+while product < 100:
+    product *= 10 
+    print(f"Product: {product:.2f}")
+    
+    
+print("Loop finished. Product is now greater than or equal to 100.")
+
+# This is a loop that will allow the user to enter a number and multiply it by 10
